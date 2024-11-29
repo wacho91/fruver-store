f4dba64b31332d0da22e01c6af9b31f7734202af
wacho91 <criskol.71@gmail.com>
2024-11-29 10:55:04 -0500

    Configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#de2c4d",
+        secondary: "#fb923c",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        averia: ["Averia Serif Libre", "serif"],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
   plugins: [],
 }
