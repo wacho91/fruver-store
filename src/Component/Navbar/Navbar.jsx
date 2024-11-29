8176f2ddbc6bc2c7484d4e007010ee8e2bf88c35
wacho91 <criskol.71@gmail.com>
2024-11-29 11:53:15 -0500

    Style Component Complete

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -30,26 +30,26 @@ const NavLnks = [
 
 const Navbar = () => {
   return (
-    <nav>
-        <div>
+    <nav className="md:pt-4 relative z-30 bg-white">
+        <div className="container py-4 flex justify-between items-center">
             <div className="text-2xl flex items-center gap-2 font-bold">
                 <p className="text-primary">FRUVER</p>
                 <p className="text-secondary">STORE</p>
             </div>
-        </div>
-        <div className="hidden md:block">
-            <ul className="flex items-center gap-6 text-gray-600">
-                {NavLnks.map((index) => (
-                    <li key={index.id}>
-                        <a 
-                            href={index.path}
-                            className="inline-block py-1 px-3 hover:text-red-500 hover:shadow-[0_3px_0_-1px_#ef4444] font-semibold"
-                        >
-                            {index.name}
-                        </a>
-                    </li>
-                ))}
-            </ul>
+            <div className="hidden md:block">
+                <ul className="flex items-center gap-6 text-gray-600">
+                    {NavLnks.map((index) => (
+                        <li key={index.id}>
+                            <a 
+                                href={index.path}
+                                className="inline-block py-1 px-3 hover:text-red-500 hover:shadow-[0_3px_0_-1px_#ef4444] font-semibold"
+                            >
+                                {index.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     </nav>
   )
